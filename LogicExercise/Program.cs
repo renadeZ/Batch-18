a76ce2148c4dd66f77f23422126e93cf14acb43a
renadeZ <mzidanenaufalr@gmail.com>
2026-03-31 13:56:04 +0700

    +Logic Exercise

---
+++ LogicExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int x = 1; x <= n; x++)
+{
+    if (x % 3 == 0 && x % 5 == 0)
+    {
+        Console.Write("foobar");
+    }
+    else if (x % 3 == 0)
+    {
+        Console.Write("foo");
+    }
+    else if (x % 5 == 0)
+    {
+        Console.Write("bar");
+    }
+    else
+    {
+        Console.Write(x);
+    }
+    Console.Write(" ");
+}
