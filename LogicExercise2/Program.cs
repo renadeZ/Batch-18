cd6ceb26385f4be314db667e637beffc9b60e4ac
renadeZ <mzidanenaufalr@gmail.com>
2026-04-06 08:09:52 +0700

    +LogicExercise2

---
+++ LogicExercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int x = 1; x <= n; x++)
+{
+    if(x % 3 == 0)
+        Console.Write("foo");
+    if(x % 5 == 0)
+        Console.Write("bar");
+    if(x % 7 == 0)
+        Console.Write("jazz");
+    if(x % 3 != 0 && x % 5 != 0 && x % 7 != 0)
+        Console.Write(x);
+    Console.WriteLine();
+}
+
